328eac6007eaf25efaa18003705dd8e6db1ad0f3
Rek-1 <103595028+Rek-1@users.noreply.github.com>
2025-03-28 16:14:49 +0300

    Changed admiin nav

--- src/app/inventory/components/navbar.tsx
+++ src/app/inventory/components/navbar.tsx
@@ -240,7 +240,7 @@ const Navbar = () => {
           </button>
           <button
             className="px-4 py-2 bg-blue-400 hover:bg-blue-800 rounded-t-lg transition-colors duration-200"
-            onClick={() => router.push('/dashboard')}
+            onClick={() => router.push('/inventory-page')}
             aria-label="Navigate to inventory"
           >
             Inventory
